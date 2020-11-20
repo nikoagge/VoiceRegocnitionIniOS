d4c30c2296872c4f6ce0a0c905ed0081e0ae011a
nikolas <nikoagge@gmail.com>
2020-11-20 19:30:19 +0200

    First general cleanup and organised project based on MVC-S design pattern architecture

---
+++ VoiceRecognitionIniOS/Home/Controller/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  VoiceRecognitionIniOS
+//
+//  Created by Nikolas Aggelidis on 20/11/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
